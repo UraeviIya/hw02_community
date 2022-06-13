f0f6f9aa19413a426f59486775f39fddee26ec05
Илья Ураев <uraevilya@yandex.ru>
2022-06-13 13:51:34 +0300

    add changed  yatube/posts/views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,10 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Group, Post, User
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request):
     posts = Post.objects.all()
     paginator = Paginator(posts, 10)
