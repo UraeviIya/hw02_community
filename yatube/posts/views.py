1b4278533788a986c7494cedf3149223cf5d7815
Илья Ураев <uraevilya@yandex.ru>
2022-06-13 13:37:25 +0300

    add changed header

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from .models import Group, Post
+from .models import Group, Post, User
 
 
 def index(request):
@@ -9,8 +9,8 @@ def index(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
+
         'page_obj': page_obj,
-        'posts': posts,
     }
     return render(request, 'posts/index.html', context)
 
@@ -27,3 +27,31 @@ def group_posts(request, slug):
         'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    author_posts = author.posts.all()
+    posts_count = author.posts.count()
+    paginator = Paginator(author_posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'posts': author_posts,
+        'posts_count': posts_count,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    posts_count = post.author.posts.count()
+    title = post.text[0:30]
+    context = {
+        'posts': post,
+        'posts_count': posts_count,
+        'title': title
+    }
+    return render(request, 'posts/post_detail.html', context)
